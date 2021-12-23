5ae48d848a00506a3d2eed09dbd4b91f632e7996
alfian <alfianpratama@hotmail.com>
2021-12-24 03:00:47 +0700

    init

---
+++ 07_failures.py
@@ -0,0 +1,47 @@
+import json
+import requests
+import pandas as pd
+from datetime import datetime, timedelta
+from prefect import task, Flow, Parameter
+
+@task
+def extract(url: str) -> dict:
+    res = requests.get(url)
+    if not res:
+        raise Exception('No Data Fetched!')
+    return json.loads(res.content)
+
+@task(max_retries=10, retry_delay=timedelta(seconds=10))
+def transform(data: dict) -> pd.DataFrame:
+    transformed = []
+    for user in data:
+        transformed.append({
+            'ID' : user['id'],
+            'Name' : user['name'],
+            'Username' : user['username'],
+            'Email' : user['email'],
+            'Address' : f"{user['address']['street']}, {user['address']['suite']}, {user['address']['city']}",
+            'PhoneNumber' : user['phone'],
+            'Company' : user['company']['name']
+        })
+    return pd.DataFrame(transformed)
+
+@task
+def load(data: pd.DataFrame, path: str) -> None:
+    data.to_csv(path_or_buf = path, index=False)
+    
+
+def prefect_flow():
+    with Flow(name='simple_etl_pipeline') as flow:
+        param_url = Parameter(name='p_url', required=True)
+
+        users = extract(url=param_url)
+        df_users = transform(users)
+        load(data=df_users, path=f'data/users_{int(datetime.now().timestamp())}.csv')
+    return flow
+
+if __name__ == '__main__':
+    flow = prefect_flow()
+    flow.run(parameters={
+        'p_url': 'https://jsonplaceholder.typicode.com/EMPTY'
+    })
